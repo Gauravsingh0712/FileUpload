865a5217b67949775b4f9b70460120e10ee91390
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-04-21 23:07:27 +0530

    completed file upload project

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -28,9 +28,13 @@ exports.localFileUpload = async (req, res) => {
     }
 }
 
-async function uploadFileToCloudinary(file, folder) {
-    const options = { folder }
-    return await cloudinary.uploader.upload(file.tempFilePath,)
+async function uploadFileToCloudinary(file, folder, quality) {
+    const options = { folder };
+    if (quality) {
+        options.quality = quality;
+    }
+    options.resource_type = "auto"
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 function isFileTypeSupported(type, supportedTypes) {
     return supportedTypes.includes(type);
@@ -84,5 +88,107 @@ exports.imageUpload = async (req, res) => {
             message: 'Something went wrong',
         });
 
+    }
+}
+
+//video upload handler function
+exports.videoUpload = async (req, res) => {
+    try {
+        // Fetch Data 
+        const { name, tags, email } = req.body;
+        console.log(name, tags, email);
+
+        const videoFile = req.files.videoFile;
+
+        // Validation 
+        const supportedTypes = ["mp4", "mov"];
+        const fileType = videoFile.name.split(".")[1].toLowerCase();
+
+        // HW - File Maximum 5MB
+        if (!isFileTypeSupported(fileType, supportedTypes)) {
+            res.status(400).json({
+                success: false,
+                message: "File type not supported"
+            })
+        }
+
+        // Supported 
+        // File Upload to the Cloudinary 
+        const response = await uploadFileToCloudinary(videoFile, "Codehelp");
+
+        // Upload To DB
+        const vidFile = new File({
+            name,
+            tags,
+            email,
+            videoUrl: response.secure_url
+        })
+
+        const file = await vidFile.save();
+
+        res.status(200).json({
+            success: true,
+            message: "video file uploaded successfully",
+            file: file
+        })
+    }
+    catch (err) {
+        console.error(err)
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong"
+        })
+    }
+}
+
+exports.imageReducer = async (req, res) => {
+    try {
+
+        const { name, tags, email } = req.body;
+        console.log(name, tags, email);
+
+        // Fetch file 
+        const imageFile = req.files.imageFile;
+        console.log(imageFile);
+
+        const supportedTypes = ["png", "jpg", "jpeg"];
+        const fileType = imageFile.name.split('.')[1].toLowerCase();
+
+        // Check file type is supported or not 
+        if (!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success: false,
+                message: "File type not supported"
+            })
+        }
+
+        // Upload to Cloudinary
+        // HW - Decrease size by height and width 
+        const response = await uploadFileToCloudinary(imageFile, "Codehelp", 50);
+        console.log(response)
+
+
+        // Upload to DB 
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url
+        })
+
+
+        res.status(200).json({
+            success: true,
+            message: "File uploaded successfully",
+            file: fileData
+        })
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong"
+        })
     }
 }
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //import controllers
-const { localFileUpload, imageUpload } = require('../controllers/fileUpload');
+const { localFileUpload, imageUpload, videoUpload, imageReducer } = require('../controllers/fileUpload');
 
 //api routes
 // router.post('/imageUpload', imageUpload);
@@ -10,5 +10,7 @@ const { localFileUpload, imageUpload } = require('../controllers/fileUpload');
 // router.post('/image-reducer', imageReducerUpload);
 router.post('/localFileUpload', localFileUpload);
 router.post('/imageUpload', imageUpload);
+router.post('/videoUpload', videoUpload);
+router.post('/imageReducer', imageReducer);
 
 module.exports = router
