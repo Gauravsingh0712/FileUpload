cfba7e69b85d4ba3bcff8e245ab936c8f6392f17
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-04-22 21:56:05 +0530

    added node mailer

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
     name: {
@@ -12,6 +14,35 @@ const fileSchema = new mongoose.Schema({
     },
     email: {
         type: String
+    }
+})
+
+// nodemailer
+fileSchema.post("save", async function (doc) {
+    try {
+        console.log("DOC : ", doc)
+
+        // transporter
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            },
+        })
+
+        // send mail 
+        const info = await transporter.sendMail({
+            from: 'From Priyansh',
+            to: doc.email,
+            subject: "New File Uploaded to Cloudinary",
+            html: `<h2>File Uploaded</h2> <br> view now - <a href="${doc.fileUrl}">CLick Here</a>`
+        })
+
+        console.log("Info : ", info)
+    }
+    catch (err) {
+        console.log(err);
     }
 })
 
